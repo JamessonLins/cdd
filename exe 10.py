c55aba7878ac6b4d909d008150fca08ddc3b8ff7
JamessonLins <124738358+JamessonLins@users.noreply.github.com>
2023-04-17 11:28:06 -0300

    Update exe 10.py

--- exe 10.py
+++ exe 10.py
@@ -1,15 +1,3 @@
-'''N1 = int(input("Informe o valor da primeira nota? \n"))
-N2 = int(input("Informe o valor da segunda nota? \n"))
-
-while N1 < 0 or  N1 > 10:
-    N1 = int(input("Informe o valor da primeira nota? \n"))
-    print(f"Os valores informados nao sao valido {N1}")
-while N2 < 0 or  N2 > 10:
-    N2 = int(input("Informe o valor da segunda nota? \n"))
-    print(f"Os valores informados nao sao validos  {N2}")
-
-print(f"a media da nota do aluno é {(N1+N2)/2}")
-'''
 n1 = int(input("Informe o valor da primeira nota? \n"))
 n2 = int(input("Informe o valor da segunda nota? \n"))
 add="S/s"
