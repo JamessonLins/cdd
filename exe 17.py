4187e7f562a6ff036186bb0ab3effc19c8713efd
JamessonLins <124738358+JamessonLins@users.noreply.github.com>
2023-04-25 11:17:53 -0300

    Add files via upload

---
+++ exe 17.py
@@ -0,0 +1,7 @@
+notas=[]
+soma=0
+for x in range(5):
+    notas.appende(input("Informe a nota dos 5 alunos?"))
+for y in range(notas):
+    soma +=notas[x]
+    print(f"A soma das notas são {soma}")
