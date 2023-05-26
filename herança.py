491912007b14ca0efc1e8ed8bbb1fd0822f1fdcc
Jamesson lins <124738358+JamessonLins@users.noreply.github.com>
2023-05-26 11:52:58 -0300

    Add files via upload

--- herança.py
+++ herança.py
@@ -2,6 +2,8 @@ class Animal():
     def __init__(self,Nome,Cor):
         self.Nome=Nome
         self.Cor=Cor
+    def EmitirSom(self):
+        print(f"O {self.Nome} emitiu som e é da cor {self.Cor}")
 
     def comer(self):
         print(f"o {self.Nome} foi comer...")
@@ -9,14 +11,19 @@ class Animal():
 class Gato(Animal):
     def __init__(self,Nome,Cor):
         super(). __init__(Nome,Cor)
-
     def EmitirSom(self):
-        print(f"O {self.Nome} foi miando... e é da cor {self.Cor}")
-
+        print(f"O {self.Nome} miou... e é da cor {self.Cor}")
 
 class Cachorro(Animal):
     def __init__(self, Nome, Cor):
         super().__init__(Nome, Cor)
+    def EmitirSom(self):
+        print(f"O {self.Nome} latil ... e é da cor {self.Cor}")
 
-    def EmitirSomC (self):
-        print(f"O {self.Nome} foi latindo... e é da cor {self.Cor}")
+class Vaca(Animal):
+    def __init__(self, Nome, Cor):
+        super().__init__(Nome, Cor)
+
+v1 = Vaca("talvez","branca")
+v1.comer()
+v1.EmitirSom()
