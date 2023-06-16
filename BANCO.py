6484d0fb6691bf05839edd1ec681cf3407f8d50b
Jamesson lins <124738358+JamessonLins@users.noreply.github.com>
2023-06-16 11:37:22 -0300

    Add files via upload

--- BANCO.py
+++ BANCO.py
@@ -8,6 +8,7 @@ banco = mysql.connector.connect(
 )
 print(banco)
 
+
 meucursor=banco.cursor()
 pesquisa = 'select * from alunos;'
 meucursor.execute(pesquisa)
@@ -15,10 +16,19 @@ resultado=meucursor.fetchall()
 
 for x in resultado:
     print(x)
+#meucursor.close()
+#banco.close()
 
+#inserindo dados na tabela
+nome1='Teste'
+cpf='04123658974'
+telefone='81994512365'
+media=9.5
+Fk_Turma=2
 cursor=banco.cursor()
-sql="delete from alunos where matricula=9;"
-cursor.execute(sql)
+sql="insert into alunos (nome,cpf,Telefone,Media,FK_Turma) values(%s,%s,%s,%s,%s)"
+data=(nome1,cpf,telefone,media,Fk_Turma)
+cursor.execute(sql,data)
 banco.commit()
 userid=cursor.lastrowid
 print(userid)
